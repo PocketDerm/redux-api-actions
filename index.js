bb01f92109204ea4d78150e161bccb29b739330a
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-04-19 13:38:27 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+import request from 'superagent';
+import Promise from 'promise';
+
+export const ASYNC_CALL_STARTED = 'ASYNC_CALL_STARTED';
+
+export function asyncStarted() {
+  return {
+    type: ASYNC_CALL_STARTED
+  };
+}
+
+export function asyncRequest(url, data = {}, headers = {}) {
+  return new Promise((resolve, reject) => {
+    request.get(url)
+      .query(data)
+      .set(headers)
+      .end((err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res.body);
+        }
+      }
+    );
+  });
+}
+
+export function postRequest(url, data, headers = {}) {
+  return new Promise((resolve, reject) => {
+    request.post(url).send(data)
+    .set(headers)
+    .end((err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.body);
+      }
+    });
+  });
+}
+
+export function putRequest(url, data, headers = {}) {
+  return new Promise((resolve, reject) => {
+    request.put(url).send(data)
+    .set(headers)
+    .end((err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.body);
+      }
+    });
+  });
+}
+
+export function deleteRequest(url, headers = {}) {
+  return new Promise((resolve, reject) => {
+    request.del(url).send(data)
+    .set(headers)
+    .end((err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.body);
+      }
+    });
+  });
+}
