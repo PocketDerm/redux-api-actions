713129d824cf869e07f618b8d3ecaaf31e4953fc
j-katzen <alto50@gmail.com>
2016-12-15 15:56:56 -0800

    add module exports plugin

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,7 @@ module.exports = {
       {
         test:  /\.jsx$|\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react', 'stage-0']
-        }
+        exclude: /node_modules/
       }
     ]
   },
