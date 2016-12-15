e3c92299325efb3c6f4af80bf8b4bc55161396eb
j-katzen <alto50@gmail.com>
2016-12-15 15:13:48 -0800

    ditch sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ var path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: { path: __dirname, filename: 'dist/bundle.js' },
-  devtool: "cheap-module-source-map",
   libraryTarget: "umd",
   module: {
     loaders: [
