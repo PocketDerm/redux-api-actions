b6958d78a80d016724576d3afbb655478715340f
j-katzen <alto50@gmail.com>
2016-12-15 15:26:55 -0800

    updating library again

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: { path: __dirname, filename: 'dist/bundle.js' },
+  library: "redux-api-actions",
   libraryTarget: "umd",
   module: {
     loaders: [
